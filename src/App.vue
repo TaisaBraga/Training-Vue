71df6fcd418c9d11ae55faa65f983dc5112b03a5
Taisa Braga <taisabraga.adm@gmail.com>
2022-03-15 19:41:56 -0300

    create navegation bar

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,46 @@
   <div>
     <v-app>
       <v-app-bar
-      color="deep-purple accent-4"
+      color="green darken-1"
       app
       dark
     >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
       <v-toolbar-title>Campeonato Brasileiro ⚽</v-toolbar-title>
 
     </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="green--text text--lighten 2"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Início</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-soccer</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Classificação</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
     </v-app>
+    
     <v-main></v-main>
   </div>
   
@@ -23,7 +53,10 @@ export default {
   name: 'App',
 
   data(){
-
+    return{
+      drawer: false,
+      group: null
+    }
   }
 }
 </script>
