2864049bb48e48ccd5b071b2ca2d9d17e4e80518
Taisa Braga <taisabraga.adm@gmail.com>
2022-03-16 20:17:47 -0300

    work table list

---
+++ src/components/TableList.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">
+            Clube
+          </th>
+          <th class="text-left">
+            Pontos
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in classification"
+          :key="item.clube"
+        >
+          <td>{{ item.clube }}</td>
+          <td>{{ item.pontos }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+<script>
+export default {
+  name: 'TableList',
+  props:{
+    clubs: {
+      type: Array,
+      required: true
+    }
+  }
+
+}
+</script>
