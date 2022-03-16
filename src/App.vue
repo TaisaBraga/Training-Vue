09c2c9e4c13455f8fdcb2b34375a9fa6d1d6582c
Taisa Braga <taisabraga.adm@gmail.com>
2022-03-16 19:03:45 -0300

    fix home page component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,72 @@
 <template>
-  <AppBar/>
+  <div id="App">
+   
+       <v-app>
+      
+      <v-app-bar
+      color="green darken-1"
+      app
+      dark
+    >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
-  
+      <v-toolbar-title>Campeonato Brasileiro ⚽</v-toolbar-title>
+
+    </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="green--text text--lighten 2"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Início</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-soccer</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Classificação</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+     <HomePage/>
+    </v-app>
+  </div>
 </template>
 
 <script>
-import AppBar from './components/Vue-app-bar'
 
-
+import HomePage from './components/HomePage.vue'
 
 export default {
   name: 'App',
-  components: {
-    AppBar,
 
+  components: {
+    HomePage,
+  },
+
+  data(){
+    return{
+      drawer: false,
+      group: null
+    }
   }
 }
 </script>
+
 <style scoped>
 
 </style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,36 @@
+<template>
+      <div>
+        <v-container>
+        <h2 class="title-home">Brasileirão Série A</h2>
+        <v-layout align-center justify-center>
+          <v-img class="home-image"
+          src="https://diariodonordeste.verdesmares.com.br/image/contentid/policy:1.2974821:1596806515/Serie-A.jpg"></v-img>
+        </v-layout>
+        <p class="text-home">A Série A do Campeonato Brasileiro de Futebol de 2020, oficialmente Brasileirão Assaí - Série A 2020 por motivos de patrocínio, é a 64.ª edição da principal divisão do futebol brasileiro. A disputa tem o mesmo regulamento dos anos anteriores, quando foi implementado o sistema de pontos corridos.</p>
+        </v-container>
+      </div>
+</template>
+<script>
+export default {
+  name: 'HomePage',
+}
+
+</script>
+<style scoped>
+.title-home {
+  font-size: 40px;
+  margin-top: 10vh;
+  text-align: center;
+}
+
+.home-image{
+  border-radius: 1%;
+  max-width: 70%;
+}
+
+.text-home{
+  font-size: 20px;
+  text-align: justify;
+  padding: 50px;
+}
+</style>
