cbb6ab03dacbd384660ad525a427060db9bddf04
Taisa Braga <taisabraga.adm@gmail.com>
2022-03-16 20:10:11 -0300

    create Club List component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomePage from '../views/HomePage'
+import ClubsList from '../views/ClubsList'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomePage
+  },
+  {
+    path: '/',
+    name: 'classification',
+    component: ClubsList
   }
 ]
 
---
+++ src/views/ClubsList.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <h2 class="text-h5 text-center mb-3 mt-5">Classificação dos Clubes</h2>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      clubsList: []
+    }
+  },
+  created(){
+    fetch('https://hackthon-decola.firebaseio.com/clubes-lista.json')
+    .then(response => response.jason())
+    .then(json => {
+      this.clubsList = json
+    })
+  }
+  
+}
+</script>
+
+<style scoped>
+
+</style>
